dadc912c675a27d4f5a3b3e5d9884ed8a8d64096
MtflML <128968935+MtflML@users.noreply.github.com>
2024-04-24 00:14:09 +1000

    Update init.go

--- init.go
+++ init.go
@@ -5,6 +5,7 @@ import (
 	"deeplx-local/domain"
 	"fmt"
 	"github.com/imroc/req/v3"
+	"golang.org/x/sync/errgroup"
 	"log"
 	"net/http"
 	"os"
@@ -12,7 +13,6 @@ import (
 	"os/signal"
 	"strconv"
 	"strings"
-	"sync"
 	"syscall"
 	"time"
 )
@@ -26,7 +26,7 @@ var (
 	}
 )
 
-// getValidURLs 从文件中读取并处理URL，返回可用URL列表
+// getValidURLs 从文件中读取并处理URL
 func getValidURLs() []string {
 	content, err := os.ReadFile("url.txt")
 	if err != nil {
@@ -34,43 +34,54 @@ func getValidURLs() []string {
 	}
 
 	urls := strings.Split(string(content), "\n")
-
-	var wg sync.WaitGroup
-	var mutex sync.Mutex
-	validURLs := make([]string, 0)
-
-	checkURL := func(url string) {
-		defer wg.Done()
-
-		if !strings.HasSuffix(url, "/translate") {
-			url += "/translate"
+	for i := range urls {
+		if !strings.HasSuffix(urls[i], "/translate") {
+			urls[i] += "/translate"
 		}
-		if !strings.HasPrefix(url, "http") {
-			url = "http://" + url
-		}
-
-		availability, err := checkURLAvailability(url)
-		if err != nil {
-			log.Printf("Error checking URL %s: %s\n", url, err)
-			return
-		}
-
-		if availability {
-			mutex.Lock()
-			validURLs = append(validURLs, url)
-			mutex.Unlock()
+		if !strings.HasPrefix(urls[i], "http") {
+			urls[i] = "http://" + urls[i]
 		}
 	}
+	// 去重
+	distinctURLs(&urls)
+	// 保存处理后的URL
+	os.WriteFile("url.txt", []byte(strings.Join(urls, "\n")), 0600)
 
-	wg.Add(len(urls))
+	eg := errgroup.Group{}
+	validList := make([]string, 0, len(urls))
+
+	// 并发检查URL可用性
 	for _, url := range urls {
-		go checkURL(url)
+		eg.Go(func() error {
+			if availability, err := checkURLAvailability(url); err == nil && availability {
+				validList = append(validList, url)
+			}
+			return err
+		})
+	}
+	if err := eg.Wait(); err != nil {
+		log.Printf("error: %s\n", err)
 	}
 
-	wg.Wait()
+	log.Printf("available urls count: %d\n", len(validList))
+	return validList
+}
 
-	log.Printf("Available URLs count: %d\n", len(validURLs))
-	return validURLs
+// distinctURLs 去重
+func distinctURLs(urls *[]string) {
+	if len(*urls) == 0 {
+		return
+	}
+	hashset := make(map[string]struct{})
+	for i := 0; i < len(*urls); i++ {
+		if _, ok := hashset[(*urls)[i]]; ok {
+			copy((*urls)[i:], (*urls)[i+1:])
+			*urls = (*urls)[:len(*urls)-1]
+			i--
+		} else {
+			hashset[(*urls)[i]] = struct{}{}
+		}
+	}
 }
 
 // checkURLAvailability 检查URL是否可用
@@ -78,14 +89,14 @@ func checkURLAvailability(url string) (bool, error) {
 	var result domain.TranslateResponse
 	response, err := client.R().SetBody(&validReq).SetSuccessResult(&result).Post(url)
 	if err != nil {
+		//log.Printf("error: url:[%s] %s\n", url, err)
 		return false, err
 	}
 	defer response.Body.Close()
-
 	return "我爱你" == result.Data, nil
 }
 
-// exit 监听退出信号并优雅关闭HTTP服务器
+// 监听退出
 func exit(engine *http.Server) {
 	osSig := make(chan os.Signal, 1)
 	quit := make(chan bool, 1)
@@ -93,20 +104,18 @@ func exit(engine *http.Server) {
 	signal.Notify(osSig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	go func() {
 		sig := <-osSig
-		fmt.Println("Received exit signal:", sig)
-
+		fmt.Println("收到退出信号: ", sig)
+		// 退出web服务
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
 		if err := engine.Shutdown(ctx); err != nil {
-			fmt.Println("Web service shutdown failed:", err)
+			fmt.Println("web服务退出失败: ", err)
 		}
 		quit <- true
 	}()
-
 	<-quit
-	fmt.Println("Service PID:", os.Getpid())
-	fmt.Println("Service has exited.")
-
+	fmt.Println("服务 PID 为: ", os.Getpid())
+	fmt.Println("服务已退出")
 	// 查杀
 	exec.Command("killall", "main", strconv.Itoa(os.Getpid())).Run()
 	// 自杀
